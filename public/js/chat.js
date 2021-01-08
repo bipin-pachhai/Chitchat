98ea0763e32d54bcafe9117d57f059514ac16b49
bpachhai <bpachhai@uno.edu>
2021-01-08 15:01:01 -0600

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -62,6 +62,25 @@ var messages = document.getElementById("messages");
     });
 })();
 
- function scrollToBottom(){
+function scrollToBottom(){
         $(".messages").animate({ scrollTop: $(document).height()+$(window).height() },500);
+    }
+function timeSince(timeStamp) {
+      var now = new Date(),
+        secondsPast = (now.getTime() - timeStamp) / 1000;
+      if (secondsPast < 60) {
+        return parseInt(secondsPast) + 's';
+      }
+      if (secondsPast < 3600) {
+        return parseInt(secondsPast / 60) + 'm';
+      }
+      if (secondsPast <= 86400) {
+        return parseInt(secondsPast / 3600) + 'h';
+      }
+      if (secondsPast > 86400) {
+        day = timeStamp.getDate();
+        month = timeStamp.toDateString().match(/ [a-zA-Z]*/)[0].replace(" ", "");
+        year = timeStamp.getFullYear() == now.getFullYear() ? "" : " " + timeStamp.getFullYear();
+        return day + " " + month + year;
+      }
     }
